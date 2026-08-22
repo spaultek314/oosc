d46d7795e6913f055b8bb544fbc905de3013a148
sresthangshu pal <238139285+spaultek314@users.noreply.github.com>
2026-08-22 19:43:41 +0530

    feat: add application bootstrap

---
+++ src/main/java/com/oosc/AgentReliabilityApplication.java
@@ -0,0 +1,11 @@
+package com.oosc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentReliabilityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AgentReliabilityApplication.class, args);
+    }
+}
